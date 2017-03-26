4c82a300f42c1b263977ea82fe48837e34bba521
mikemaciolek <MMAC@Mikes-MacBook-Pro.local>
2017-03-26 16:33:36 -0500

    More updates

--- MoneyMove.py
+++ MoneyMove.py
@@ -1,12 +1,38 @@
 # import modules used here -- sys is a very standard one
 import sys
 import locale
+import os
 
 locale.setlocale(locale.LC_ALL, 'en_US')
 
-#class Movement:
-#    def quack(self): return self.strings['quack']
-#    def bark(self): return self.strings['bark']
+class Movement:
+
+	def __init__(self):
+		self.myList = [["Empty",0.0,"biweekly", "income"]]
+
+	def add(self):
+		print("add called")
+		
+	def remove(self):
+		print("Remove called")
+
+	def displayList(self):
+		for i in self.myList:
+			print("---------- "+ str(self.myList.index(i)) + " ----------")
+			print(i)
+	
+	def displayMenu(self):
+		
+		os.system('clear')
+		self.displayList()
+		print("")
+		print("Please select from the following options:")
+		print("")
+		print("       1 - Add")
+		print("       2 - Remove")
+		print("       0 - Back")
+		print("")
+		
 
 class Account:
 	
@@ -29,6 +55,8 @@ class Account:
 	def getName(self):
 		return self.accountName	
 		
+	def calculate(self):
+		return 1971
 
 class Menu:
     
@@ -36,12 +64,17 @@ class Menu:
     def __init__(self, account):
     	self.accountBalance = account.getBalance()
     	
-    def display(self):
+    def displayMain(self):
     	print("")
     	print("Welcome to MoneyMove v.1. Your account balance is " + locale.currency(self.accountBalance) + ".")
     	print("Please select from the following options:")
     	print("")
-
+    	
+    	
+    	
+    	print("       0 - Quit")
+    	print("")
+		
 
 # Gather our code in a main() function
 def main():
@@ -55,21 +88,33 @@ def main():
     account.setName("Recurring")
     account.setBalance(0)
     
+    movement = Movement()
+    
     while (run != 0):
     	
     	mainMenu = Menu(account)
-    	mainMenu.display()
+    	mainMenu.displayMain()
     
-    	run = int(raw_input("Enter something: "))
+    	run = int(raw_input(">>>>>> "))
     	
     	if (run == 1):
     		account.setBalanceFromTerm()
     		
     	elif (run == 2):
-    		print("Made it to 2")
+    		movement.displayMenu()
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    	elif (run == 9):
+    		print(str(account.calculate()))
 		
     
-    print ("Exiting now")
+    print ("Exiting. Thank you.")
+    print("")
 
 
 
